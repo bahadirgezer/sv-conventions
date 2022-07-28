56eb10ecbd837048ae644ca7a80e7d2280956d2e
bahadirgezer <bahadirgezerbg@gmail.com>
2022-07-28 03:01:07 +0300

    Account service and controller done.

--- src/main/java/compaintvar/convention/controller/AccountController.java
+++ src/main/java/compaintvar/convention/controller/AccountController.java
@@ -1,4 +1,116 @@
 package compaintvar.convention.controller;
 
+import com.sun.istack.NotNull;
+import compaintvar.convention.dto.AccountDTO;
+import compaintvar.convention.entity.Account;
+import compaintvar.convention.exceptions.ResourceNotFoundException;
+import compaintvar.convention.service.AccountService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/account")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AccountController {
+    AccountService accountService;
+
+    /**
+     * A1: ID'si parametre olarak verilen Account'u buluyor.
+     *
+     * @param id Istenen Account'un ID'si
+     * @return Istenen Account'un DTO'su
+     */
+    @GetMapping
+    public ResponseEntity<AccountDTO> getAccountById(@RequestParam Long id,
+            @RequestParam(defaultValue = "5") Integer commentLimit) {
+        AccountDTO accountDTO = accountService.getAccountById(id, commentLimit);
+        if (accountDTO == null) {
+            log.debug(String.format("Could not get account with ID: %d.", id));
+            throw new ResourceNotFoundException(
+                    String.format("Account with ID: %d not found.", id));
+        }
+
+        return ResponseEntity.ok().body(accountDTO);
+    }
+
+    /**
+     * A2: Request body olarak gelen Account DTO'sunu veritabanina kaydeder.
+     * Eger olusturma islemi basarili olursa kaydedilen Account DTO'sunu
+     * HTTP 201 created status ile doner.
+     *
+     * @param newAccountDTO olusturulmak istenen Account DTO'su
+     * @return kaydedilen Account DTO'su
+     */
+    @PostMapping
+    public ResponseEntity<AccountDTO> createAccount(@RequestBody @NotNull AccountDTO newAccountDTO) {
+        AccountDTO accountDTO = accountService.createAccount(newAccountDTO);
+        if (accountDTO == null) {
+            log.debug(String.format(
+                    "Could not create new account with ID: %d.", newAccountDTO.getId()));
+            throw new ResourceNotFoundException(
+                    String.format("Account with ID: %d not found.", newAccountDTO.getId()));
+        }
+        return new ResponseEntity<AccountDTO>(accountDTO, HttpStatus.CREATED);
+    }
+
+    /**
+     * A3: ID'si verilen Account'un username'ini gunceller.
+     * Yeni username'in biricik olup olmadigi kontrol edilir.
+     *
+     * @param id guncellenmek istenen Account'un ID'si
+     * @param username yeni username
+     * @return Guncellenen Account'un kaydedilen DTO'su
+     */
+    @PutMapping
+    public ResponseEntity<AccountDTO> updateAccountUsername(@RequestParam Long id,
+                                                            @RequestParam String username) {
+        AccountDTO accountDTO = accountService.updateUsername(id, username);
+        if (accountDTO == null) {
+            log.debug(String.format(
+                    "Could not update username of the account with ID: %d.", id));
+            throw new ResourceNotFoundException(
+                    String.format("Account with ID: %d not found.", id));
+        }
+        return new ResponseEntity<AccountDTO>(accountDTO, HttpStatus.ACCEPTED);
+    }
+
+    /**
+     * A4: ID'si verilen Account'un email'ini gunceller.
+     * Yeni email'in biricik olup olmadigi kontrol edilir.
+     *
+     * @param id guncellenmek istenen Account'un ID'si
+     * @param email yeni email
+     * @return Guncellenen Account'un kaydedilen DTO'su
+     */
+    @PutMapping
+    public ResponseEntity<AccountDTO> updateAccountEmail(@RequestParam Long id,
+                                                            @RequestParam String email) {
+        AccountDTO accountDTO = accountService.updateEmail(id, email);
+        if (accountDTO == null) {
+            log.debug(String.format(
+                    "Could not update email of the account with ID: %d.", id));
+            throw new ResourceNotFoundException(
+                    String.format("Account with ID: %d not found.", id));
+        }
+        return new ResponseEntity<AccountDTO>(accountDTO, HttpStatus.ACCEPTED);
+    }
+
+    /**
+     * A5: ID'si verilen Account'u database'den siler.
+     *
+     * @param id silinmek istenen Account'un ID'si
+     * @return islem basarisina gore HTTP status
+     */
+    @DeleteMapping
+    public HttpStatus deleteAccountById(@RequestParam Long id) {
+        return accountService.deleteAccount(id) ? HttpStatus.ACCEPTED : HttpStatus.NOT_ACCEPTABLE;
+    }
+
 }
--- src/main/java/compaintvar/convention/service/AccountService.java
+++ src/main/java/compaintvar/convention/service/AccountService.java
@@ -25,42 +25,7 @@ public class AccountService {
      * Parametre olarak verilen ID'ye gore databaseden Account objesi cekiyor.
      * Bu obje DTO olarak donduruluyor.
      *
-     * Default olarak maximum 5 tane comment donduruyor.
-     *
-     * @param id Istenen Account objesinin ID'si
-     * @return Istenen Account objesinin DTO'su
-     */
-    public AccountDTO getAccountById(Long id) {
-        Account account;
-
-        log.debug(String.format("Fetching account with ID: %d.", id));
-        try {
-            account = accountRepository.findAccountById(id);
-        } catch (Exception e) {
-            log.debug(String.format("Database query exception: Account fetch with ID: %d failed.", id));
-            e.printStackTrace();
-            return null;
-        }
-
-        if (account == null) {
-            log.debug(String.format("Account with ID: %d not found.", id));
-            throw new ResourceNotFoundException(String.format("Account (ID: %d) not found.", id));
-        }
-
-        return new AccountDTO(account.getId(),
-                account.getEmail(),
-                account.getUsername(),
-                account.getComments() // Butun commentleri dondurmek istemiyoruz
-                        .stream().limit(5) // 5 tane
-                        .collect(Collectors.toSet()),
-                account.getCommentCount());
-    }
-
-    /**
-     * Parametre olarak verilen ID'ye gore databaseden Account objesi cekiyor.
-     * Bu obje DTO olarak donduruluyor.
-     *
-     * Comment limiti belirlenebiliyor.
+     * Comment limiti belirlenebiliyor. Default value controllerda ayarlanmaktadir.
      *
      * @param id Istenen Account objesinin ID'si
      * @return Istenen Account objesinin DTO'su
@@ -91,6 +56,12 @@ public class AccountService {
                 account.getCommentCount());
     }
 
+    /**
+     * Verilen AccountDTO objesini database'e kayit ediyor.
+     *
+     * @param accountDTO Olusturulmak istenen Account
+     * @return Kaydedilen Account'un DTO'su
+     */
     public AccountDTO createAccount(@NotNull AccountDTO accountDTO) {
         log.debug(String.format("Creating new account (ID: %d, username: %s, email: %s).",
                 accountDTO.getId(), accountDTO.getUsername(), accountDTO.getEmail()));
@@ -122,6 +93,104 @@ public class AccountService {
                 savedAccount.getCommentCount());
     }
 
+    /**
+     * ID'si verilen Account'un username'ini guncelliyor.
+     *
+     * @param id Istenen Account objesinin ID'si
+     * @param username yeni username
+     * @return Kaydedilen Account'un DTO'su
+     */
+    public AccountDTO updateUsername(Long id, String username) {
+        Account account;
+
+        log.debug(String.format("Fetching account with ID: %d.", id));
+        try {
+            account = accountRepository.findAccountById(id);
+        } catch (Exception e) {
+            log.debug(String.format("Database query exception: Account fetch with ID: %d failed.", id));
+            e.printStackTrace();
+            return null;
+        }
+        if (account == null) {
+            log.debug(String.format("Account with ID: %d not found.", id));
+            throw new ResourceNotFoundException(String.format("Account (ID: %d) not found.", id));
+        }
+
+        account.setUsername(username);
+        if (!checkAccountUniqueness(account)) { //if account is not unique
+            return null;
+        }
+
+        Account savedAccount;
+        try {
+            savedAccount = accountRepository.save(account);
+        } catch (Exception e) {
+            log.debug(String.format(
+                    "Database query exception: Account save with ID: %d failed.", account.getId()));
+            e.printStackTrace();
+            return null;
+        }
+
+        return new AccountDTO(savedAccount.getId(),
+                savedAccount.getEmail(),
+                savedAccount.getUsername(),
+                savedAccount.getComments(),
+                savedAccount.getCommentCount());
+    }
+
+    /**
+     * ID'si verilen Account'un email'ini guncelliyor.
+     *
+     * @param id Istenen Account objesinin ID'si
+     * @param email yeni email
+     * @return Kaydedilen Account'un DTO'su
+     */
+    public AccountDTO updateEmail(Long id, String email) {
+        Account account;
+
+        log.debug(String.format("Fetching account with ID: %d.", id));
+        try {
+            account = accountRepository.findAccountById(id);
+        } catch (Exception e) {
+            log.debug(String.format("Database query exception: Account fetch with ID: %d failed.", id));
+            e.printStackTrace();
+            return null;
+        }
+        if (account == null) {
+            log.debug(String.format("Account with ID: %d not found.", id));
+            throw new ResourceNotFoundException(String.format("Account (ID: %d) not found.", id));
+        }
+
+        account.setEmail(email);
+        if (!checkAccountUniqueness(account)) { //if account is not unique
+            return null;
+        }
+
+        Account savedAccount;
+        try {
+            savedAccount = accountRepository.save(account);
+        } catch (Exception e) {
+            log.debug(String.format(
+                    "Database query exception: Account save with ID: %d failed.", account.getId()));
+            e.printStackTrace();
+            return null;
+        }
+
+        return new AccountDTO(savedAccount.getId(),
+                savedAccount.getEmail(),
+                savedAccount.getUsername(),
+                savedAccount.getComments(),
+                savedAccount.getCommentCount());
+    }
+
+    /**
+     * Parametre olarak verilen account objesinin biricikligini kontrol eder.
+     *
+     * Email ve username biricik olmali.
+     *
+     * @param account Kontrol edilmek istenen Account
+     * @return Biricik ise True degilse False
+     */
     private Boolean checkAccountUniqueness(Account account) {
         Account emailCheck;
         try {
@@ -156,4 +225,33 @@ public class AccountService {
         } //username check done
         return true;
     }
+
+    public Boolean deleteAccount(Long id) {
+        Account account;
+
+        log.debug(String.format("Fetching account with ID: %d.", id));
+        try {
+            account = accountRepository.findAccountById(id);
+        } catch (Exception e) {
+            log.debug(String.format("Database query exception: Account fetch with ID: %d failed.", id));
+            e.printStackTrace();
+            return false;
+        }
+        if (account == null) {
+            log.debug(String.format("Account with ID: %d not found.", id));
+            throw new ResourceNotFoundException(String.format("Account (ID: %d) not found.", id));
+        }
+
+        log.debug(String.format("Deleting account with ID: %d.", id));
+        try {
+            accountRepository.delete(account);
+        } catch (Exception e) {
+            log.debug(String.format("Database query exception: Account deletion with ID: %d failed.", id));
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
 }
