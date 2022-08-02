826f55f92b72bb3170c6f53970fbbf80e46c8136
bahadirgezer <bahadirgezerbg@gmail.com>
2022-08-03 01:39:21 +0300

    Initial commit

---
+++ src/main/java/com/sikayetvar/convention/ConventionApplication.java
@@ -0,0 +1,13 @@
+package com.sikayetvar.convention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConventionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConventionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sikayetvar/convention/ConventionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sikayetvar.convention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConventionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
